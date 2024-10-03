c0423f6b9d82ffb34538f3501526221fc221e930
Mridupawan Bordoloi <mridupawan503@gmail.com>
2024-10-03 14:34:42 +0530

    Update for SEO purpose

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,97 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
-import {About,Layout, ErrorPage, Footer, Github, Header,Skills,AllProjects,Educations} from './components'
-
+import React from "react";
+// import ReactDOM from "react-dom/client";
+import { hydrateRoot, createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  About,
+  Layout,
+  ErrorPage,
+  Footer,
+  Github,
+  Header,
+  Skills,
+  AllProjects,
+  Educations,
+} from "./components";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <div style={{width:'100%',height:"100vh",fontSize:"3rem",display:'flex',justifyContent:'center',alignItems:'center'}}><h1 style={{color:'black',margin:'auto'}}>We will be coming soon !</h1></div>
+    errorElement: (
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          fontSize: "3rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ color: "black", margin: "auto" }}>
+          We will be coming soon !
+        </h1>
+      </div>
+    ),
   },
 
   {
     path: "/about",
-    element: <><Header /><About /> <Skills /><Educations /><Footer /></>,
-    errorElement:<ErrorPage />
+    element: (
+      <>
+        <Header />
+        <About /> <Skills />
+        <Educations />
+        <Footer />
+      </>
+    ),
+    errorElement: <ErrorPage />,
   },
   {
     path: "/projects",
-    element: <><Header /><AllProjects /><Footer /></>,
-    errorElement:<ErrorPage />
+    element: (
+      <>
+        <Header />
+        <AllProjects />
+        <Footer />
+      </>
+    ),
+    errorElement: <ErrorPage />,
   },
   {
     // loader:{gitInfoLoader},
     path: "/github",
-    element: <><Header /><ErrorPage /><Footer /></>,
+    element: (
+      <>
+        <Header />
+        <ErrorPage />
+        <Footer />
+      </>
+    ),
     // errorElement:<ErrorPage />
   },
-
 ]);
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// )
 
+const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+} else {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
